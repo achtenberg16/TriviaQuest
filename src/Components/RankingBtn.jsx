45c5e8ff6266aff70f98452bf4007ad2d34f6cd5
achtenberg16 <achtenberg16@gmail.com>
2022-05-18 18:03:43 -0300

    feat: implementado logica de salvar ranking

--- src/Components/RankingBtn.jsx
+++ src/Components/RankingBtn.jsx
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
 import { useHistory } from 'react-router';
+import md5 from 'crypto-js/md5';
+import { getLocalStorage, setLocalStorage } from '../services/localStorage';
 
 function RankingBtn() {
   const history = useHistory();
+  const player = useSelector((state) => state.player);
+  const hashEmail = md5(player.gravatarEmail).toString();
+  const pictureUrl = `https://www.gravatar.com/avatar/${hashEmail}?d=identicon`;
+  useEffect(() => {
+    const prevRank = getLocalStorage('ranking') || [];
+    const newRank = [...prevRank, { name: player.name,
+      score:
+      player.score,
+      picture: pictureUrl }];
+    setLocalStorage(JSON.stringify(newRank));
+  }, []);
   return (
     <button
       type="button"
